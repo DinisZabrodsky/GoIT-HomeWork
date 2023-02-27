f49238b974284ebd74bc16bd1ca2bef54e872acd
Dinis PC <DinisZabrodsky@gmail.com>
2023-02-27 23:31:21 +0200

    001

--- goit-js-hw-06/js/task-10.js
+++ goit-js-hw-06/js/task-10.js
@@ -2,4 +2,42 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const control = document.querySelector('#controls');
+const boxForEl = document.querySelector('#boxes');
+const createBtn = control.querySelector('[data-create]');
+const destoroyBtn = control.querySelector('[data-destroy]');
 
+createBtn.addEventListener('click', createlist);
+destoroyBtn.addEventListener('click', clearDesk)
+
+let numberEl = 0;
+
+function createlist () {
+  const value = control.children[0].value;
+  numberEl = value;
+
+  for (i = 0, i >= value, i += 1) {
+
+    
+
+  }
+  
+
+ 
+  
+  constructor() 
+
+};
+
+
+
+function constructor() {
+
+
+  return boxEl;
+}
+
+function clearDesk () {
+  control.children[0].value = 0;
+
+}
