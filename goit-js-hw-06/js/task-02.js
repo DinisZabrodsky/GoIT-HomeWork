231645444d1a4e6325068f416abcf9f58c55003c
Dinis PC <DinisZabrodsky@gmail.com>
2023-02-27 13:32:02 +0200

    оновлення

---
+++ goit-js-hw-06/js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+const selectorEl = document.querySelector('#ingredients');
+
+const addElement = [];
+
+function addListEl() {
+  
+  
+};
+
+
